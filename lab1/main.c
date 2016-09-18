217254bb06e97118308fc5a07bf916daf179fd69
Yuwei Xiao <xyw1105@126.com>
2016-09-18 20:26:54 +0800

    [lab1] maxargs function pass test. [TODO] think about exp inside  A_printStm->u.A_expList is a A_eseqExp

--- lab1/main.c
+++ lab1/main.c
@@ -5,6 +5,40 @@
 #include "slp.h"
 int maxargs(A_stm stm);
 void interp(A_stm stm);
+
+
+int maxargs(A_stm stm) {
+	int ans = 0;
+	if(stm->kind == A_compoundStm) {
+		int a = maxargs(stm->u.compound.stm1);
+		int b = maxargs(stm->u.compound.stm2);
+		ans = a > b? a:b;
+	} else if(stm->kind == A_assignStm) {
+		A_exp exp = stm->u.assign.exp;
+		while(1) {
+			if(exp->kind != A_eseqExp) {
+				break;
+			}
+			int t = maxargs(exp->u.eseq.stm);
+			ans = ans > t? ans:t;
+			exp = exp->u.eseq.exp;
+		}
+	} else {
+		A_expList list = stm->u.print.exps;
+		while(1) {
+			ans++;
+			if(list->kind == A_lastExpList) {
+				break;
+			}
+			list = list->u.pair.tail;
+		}
+	}
+	return ans;
+}
+
+void interp(A_stm stm) {
+
+}
 /*
  *Please don't modify the main() function
  */
