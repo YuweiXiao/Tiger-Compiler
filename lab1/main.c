d47f023053bf7f0722ba36c000394213397333b5
Yuwei Xiao <xyw1105@126.com>
2016-09-18 21:29:13 +0800

    [lab1] complete interp() function. pass all test.

--- lab1/main.c
+++ lab1/main.c
@@ -1,12 +1,37 @@
+/**
+ * Author: Xiao Yuwei
+ * Description: Lab1
+ * Date: 2016.09.18
+ * Score: pass
+ */
+
 /* This file is not complete.  You should fill it in with your
    solution to the programming exercise. */
 #include <stdio.h>
+#include <string.h>
 #include "prog1.h"
 #include "slp.h"
 int maxargs(A_stm stm);
 void interp(A_stm stm);
 
+typedef struct table *Table_;
+struct table
+{
+	string id;
+	int value;
+	Table_ tail;
+};
 
+struct IntAndTable {
+	int i;
+	Table_ t;
+};
+
+Table_ interpStm(A_stm stm, Table_ t);
+struct IntAndTable interpExp(A_exp exp, Table_ t);
+
+
+//TODO problem, print( (print(3),1), 2) counts 2 or 3 ?
 int maxargs(A_stm stm) {
 	int ans = 0;
 	if(stm->kind == A_compoundStm) {
@@ -36,8 +61,94 @@ int maxargs(A_stm stm) {
 	return ans;
 }
 
-void interp(A_stm stm) {
 
+Table_ Table(string id, int value, struct table *tail) {
+	Table_ t = checked_malloc(sizeof(*t));
+	t->id = id;
+	t->value = value;
+	t->tail = tail;
+	return t;
+}
+
+/**
+ * loop up corresponding value of key in table
+ * @return     value
+ *             -1 if the key does not exist
+ */
+int lookup(Table_ t, string key) {
+	if(t == NULL) {
+		printf("\nundefine key : %s!\n", key);
+    	return -1;
+	}
+	if(strcmp(t->id, key) == 0) {
+		return t->value;
+	}
+	return lookup(t->tail, key);
+}
+
+struct IntAndTable interpExp(A_exp exp, Table_ t) {
+	struct IntAndTable ans, tmp;
+	if(exp->kind == A_idExp) {				// id expression
+		ans.i = lookup(t, exp->u.id);
+	} else if(exp->kind == A_numExp) {		// number expression
+		ans.i = exp->u.num;
+	} else if(exp->kind == A_opExp) {		// operation expression
+		// get the left and right value of operation
+		tmp = interpExp(exp->u.op.left ,t);
+		int l = tmp.i;
+		t = tmp.t;
+		tmp = interpExp(exp->u.op.right, t);
+		int r = tmp.i;
+		t = tmp.t;
+		// do operation
+		if(exp->u.op.oper == A_plus) {
+			ans.i = l + r;
+		} else if(exp->u.op.oper == A_minus) {
+			ans.i = l - r; 
+		} else if(exp->u.op.oper == A_times) {
+			ans.i = l * r;
+		} else if(exp->u.op.oper == A_div) {
+			ans.i = l / r;
+		}
+	} else {								// seq expression
+		t = interpStm(exp->u.eseq.stm, t);
+		tmp = interpExp(exp->u.eseq.exp, t);
+		t = tmp.t;
+		ans.i = tmp.i;
+	}
+	ans.t = t;								// set new table
+	return ans;
+}
+
+Table_ interpStm(A_stm stm, Table_ t) {
+	if(stm->kind == A_compoundStm) {		// compound statement
+		t = interpStm(stm->u.compound.stm1, t);
+		t = interpStm(stm->u.compound.stm2, t);
+	} else if(stm->kind == A_assignStm) {	// assign statement
+		struct IntAndTable tmp = interpExp(stm->u.assign.exp, t);
+		t = Table(stm->u.assign.id, tmp.i, tmp.t);
+	} else {								// print statement
+		A_expList list = stm->u.print.exps;
+		struct IntAndTable tmp;
+		while(1) {
+			if(list->kind == A_lastExpList) {
+				tmp = interpExp(list->u.last, t);
+				printf("%d\n", tmp.i);
+				t = tmp.t;
+				break;
+			} else {
+				tmp = interpExp(list->u.pair.head, t);
+				printf("%d ", tmp.i);
+				t = tmp.t;
+				list = list->u.pair.tail;
+			}
+		}
+	}
+	return t;
+}
+
+void interp(A_stm stm) {
+	interpStm(stm, NULL);
 }
 /*
  *Please don't modify the main() function
